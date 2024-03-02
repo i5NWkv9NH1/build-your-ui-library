e0fb95b5a0fa7a50e2b9d47ad8b63addbe046c61
i5NWkv9NH1 <sloananna326@gmail.com>
2024-03-03 08:29:32 +0900

    feat: add custom configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { URL, fileURLToPath } from 'node:url'
 
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -10,6 +11,26 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es'],
+      // the proper extensions will be added
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
